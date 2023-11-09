9613bdcf6727632450afa41a614a2f6fa572e77c
meghasyam1997 <58021202+meghasyam1997@users.noreply.github.com>
2023-11-09 09:05:49 +0530

    Update main.tf
    Dns records

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ data "aws_ami" "ami" {
 data "aws_security_group" "allow_all" {
   name = "allow-all"
 }
+
+resource "aws_route53_record" "records" {
+  for_each = var.components
+  name     = "${each.value["name"]}-${var.env}.msdevops72.online"
+  type     = "A"
+  ttl      = 30
+  zone_id  = "Z024184021A09H13X7MEW"
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
+}
